7d3d3128ff3fbd1f327293aac1fd20c20135475a
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-10 10:07:12 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+// main entry point of the program. 
 int main(void)
 {
-	char lowerCase;
-	for(lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+char lowerCase;
+for(lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
 {
-		putchar(lowerCase);
-		putchar();
-   } 
+	putchar(lowerCase);
+	putchar();
+} 
 return (0);
+
 }
