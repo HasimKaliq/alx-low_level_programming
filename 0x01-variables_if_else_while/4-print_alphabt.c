0b0e47caca83bfc9bfc59b9da3a6011b9de667a7
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-10 11:10:12 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int
+main(void)
+{
+char lowerCase;
+for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+{
+if (lowerCase == 'q' || lowerCase == 'e')
+{
+continue;
+}
+else
+{
+putchar(lowerCase);
+}
+}
+putchar('\n');
+return (0);
+}
