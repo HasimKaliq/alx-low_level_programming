fa20a18e8e023ec126fbfd5e5dde5cd555473ced
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-11 20:58:31 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-int
-main(void)
+int main(void)
 {
 int numVal;
+int alpha;
+char sixAlpha[] = "abcdef";
 for (numVal = 0; numVal < 10; numVal++)
 {
         putchar(numVal + '0');
 }
-int alpha;
-char sixAlpha[] = "abcdef";
+
 for (alpha = 0; alpha <= 5; alpha++)
 {
         putchar(sixAlpha[alpha]);
