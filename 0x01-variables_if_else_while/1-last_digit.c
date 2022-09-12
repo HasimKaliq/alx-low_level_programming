da5fc75fafcdfcd0f872b4ecfec6b7e677ca6a6b
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-12 15:55:48 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 int main(void)
-{
-	int main(void)
 {
 
   int n;
@@ -31,6 +27,4 @@ int main(void)
     }
 
   return (0);
-}
-	return (0);
 }
