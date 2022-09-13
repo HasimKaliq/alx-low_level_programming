5c7d8352a9580facdb068cb317a8bbd28abeea7e
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 14:28:31 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 int main(void) 
 {
-_putchar('_putchar');
+char _putchar[] = "_putchar"
+int i;
+for (i = 0; i <= 7; i++) 
+{
+_putchar('%c', _putchar[i]);
+}
 _putchar('\n');
 return (0);
 }
