dd1b06405899f6081b11f3a24a50d7d472d9071c
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 15:48:39 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-#include <main.h>
-
+#include "main.h
 /*
  * The program below is intended to print the letters a-z
+ * The code leaves in the method void print_alphabet(void)
+ * it has a return type of void and a parameter of void
  * in lowercase, while using the _putchar() method that leaves in the 
  * main.h file.
 */
-
-int main()
+void print_alphabet(void)
 {
   int i;
   for (i = 'a'; i <= 'z'; i++)
 {
   _putchar(i);
 }
+}
+int main()
+{
+  print_alphabet();  
   _putchar('\n');
   return (0);
 }
