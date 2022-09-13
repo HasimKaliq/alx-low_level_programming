cdf934f7f52c79df8d5bb522986cb1eae9c74c14
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 16:41:36 -0700

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,12 @@
  * main.h file.
 */
 
-void print_alphabet_x10(void)
+void
+print_alphabet_x10(void)
 {
 int i, j;
-for (j = 1; j <= 10; j++) {
+for (j = 1; j <= 10; j++)
+{
 for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
