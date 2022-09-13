ee2074a258a595076904a245d7a0ff058d067861
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 16:23:51 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,12 +14,6 @@ void print_alphabet(void)
   int i;
   for (i = 'a'; i <= 'z'; i++)
 {
-  _putchar(i);
+  _putchar('%s', i);
 }
-}
-int main()
-{
-  print_alphabet();  
-  putchar('\n');
-  return (0);
 }
