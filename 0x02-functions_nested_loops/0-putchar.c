33f1cdd45f33a40343e7641d1f7fb98089d355f3
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 14:55:25 -0700

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,6 @@
 #include "main.h"
 int main(void) 
 {
-char setOfText[] = "_putchar";
-int i;
-for (i = 0; i <= 7; i++) 
-{
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -14,7 +10,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-}
 _putchar('\n');
 return (0);
 }
