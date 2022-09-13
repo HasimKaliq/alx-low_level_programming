22cc5bdc85a3dcd668b6834a7989d49444dba329
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 16:35:51 -0700

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * The program below is intended to print the letters a-z x10
+ * The code leaves in the method void print_alphabet(void)
+ * it has a return type of void and a parameter of void
+ * in lowercase, while using the _putchar() method that leaves in the
+ * main.h file.
+*/
+void
+print_alphabet(void)
+{
+int i, j;
+for (j = 0; j <= 10; j++) {
+  for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
+}
