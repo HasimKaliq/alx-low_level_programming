d466b72106166a311e9c131f665876fdd79cefe5
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 17:17:49 -0700

    Create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ * The program below is intended to return 1 if c is a lower
+ * and return 0 if it's not.
+ * The code leaves in the method int _islower(int c)
+ * it has a return type of int and a parameter of type int
+ * in lowercase, while using the _putchar().
+*/
+int _islower(int c)
+{
+char islower = c;
+int asciiValue = ("%d", islower);
+if (asciiValue > 64 && asciiValue < 91)
+{
+return (0);
+}
+else if (asciiValue > 96 && asciiValue < 123)
+{
+return (1);
+}
+}
