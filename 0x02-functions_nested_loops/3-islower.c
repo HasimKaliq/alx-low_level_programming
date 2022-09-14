89b483c7db71df1a242b1c179743a006fce35c6b
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 17:32:43 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,19 +5,19 @@
  * The code leaves in the method int _islower(int c)
  * it has a return type of int and a parameter of type int
  * in lowercase, while using the _putchar().
- * This program below makes use of the 
+ * This program below makes use of the
  * ASCII table to determine if the character entered
- * was a lowercase or an upper case character, 
+ * was a lowercase or an upper case character,
  * this works because under the hood,
- * characters are just integers and strings 
+ * characters are just integers and strings
  * are a set of characters which means they
- * are all just a bunch of int's, all that was left to do is specify 
+ * are all just a bunch of int's, all that was left to do is specify
  * a range and use an operator on the range
  * and whola! you have your self,
  * an ASCII checker or lower or upper case checker.
 
 */
-int 
+int
 _islower(int c)
 {
 
