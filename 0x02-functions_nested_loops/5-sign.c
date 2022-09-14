447a600cd1311e6761677a6e2fd9b9dd2918f167
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 19:00:37 -0700

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,14 +15,15 @@
  * and whola! you have your self,
  * an ASCII checker or lower or upper case checker.
 */
-int print_sign(int n)
+int
+print_sign(int n)
 {
 if (n > 0)
 {
 _putchar('+');
 return (1);
 }
-else if ( n == 0)
+else if (n == 0)
 {
 _putchar('0');
 return (0);
