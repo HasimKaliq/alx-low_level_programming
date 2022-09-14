b483c078a8a41dce69b49aeba8dbed7aa2cced35
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-13 17:47:20 -0700

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -17,10 +17,10 @@
  * an ASCII checker or lower or upper case checker.
 */
 int
-_isalpha(int c)(int c)
+_isalpha(int c)
 {
 
-if (c > 64 && c < 91 || c > 96 && c < 123 )
+if ((c > 64 && c < 91) || (c > 96 && c < 123) )
 {
 return (1);
 }
