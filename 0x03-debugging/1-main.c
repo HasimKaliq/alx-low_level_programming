8084846e47597d6b89da27549d3485fc7b3221d2
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-14 22:35:11 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -7,11 +8,11 @@
 
 int main(void)
 {
-        int i;
+        /**int i;*/
 
         printf("Infinite loop incoming :(\n");
 
-        i = 0;        
+       /** i = 0;*/   
         
         /** while (i < 10)
         {
