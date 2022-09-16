0a94b4d414867a38afdfff02d245ccc33481d60c
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-15 20:33:02 -0700

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,20 +9,16 @@
  */
 int largest_number(int a, int b, int c)
 {
-	int largest;
+	int largest = b;
 
-	if (a >= b && a >= c )
+	if (a > b)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if (c > b)
 	{
-		largest = b;
-	}
-	else {
 		largest = c;
 	}
-
 
 	return (largest);
 }
