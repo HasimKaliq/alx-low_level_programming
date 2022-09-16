0c8a9e58d9a82f03685169cd3c57501b46d00eab
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-15 18:05:50 -0700

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,12 +4,13 @@
   
 int _isupper(int c) {
       
-  if (c > 64 && c < 91){
-        return (1);
-    }
-  
-  else {
-        return (0);
-    }
+if (c > 96 && c < 123)
+{
+return (0);
 }
-
+else if (c > 64 && c < 91)
+{
+return (1);
+}
+return (0);
+}
