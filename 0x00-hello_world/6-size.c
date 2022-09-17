c9b0070436ec2946766f470e49446b809fbde0c1
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-17 20:02:24 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ printf("Size of a char: %d bytes(s)\n", sizeof(char));
 printf("Size of int: %d bytes(s)\n", sizeof(int));
 printf("Size of long int: %d bytes(s)\n", sizeof(long int));
 printf("Size of long long int: %d bytes(s)\n", sizeof(long long int));
-printf("Size of double: %d bytes(s)\n", sizeof(double));
+printf("Size of float: %d bytes(s)\n", sizeof(float));
 
 return (0);
 
