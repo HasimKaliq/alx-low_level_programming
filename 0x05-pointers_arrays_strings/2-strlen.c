3e819bf66962f20e90f86e643c5f721217adb1d6
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-19 16:12:04 -0700

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,10 +4,11 @@
 /* Write a function that returns the length of a string.*/
 
 int 
-_strlen(char *s) {
-   int i;
-   for(i = 0; s[i] != '\0'; i++) {
+_strlen (char *s)
+{
+int i;
+for(i = 0; s[i] != '\0'; i++) {
        
-   }
-   return (i);
+}
+return (i);
 }
