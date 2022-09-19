a150a3ff77613fc6569e0af3bf28a196952a537b
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-19 09:24:30 -0700

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,5 @@
+include "main.h"
+
 void reset_to_98(int *n) {
-       printf("The value of the address of N is : %d\n", *n);
        *n = 98;
-       printf("The value of the address of N is now : %d\n", *n);
 }
