a833e2936d797a3e0dc2d4fcecd84a63775bfd56
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-19 21:06:25 -0700

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,15 +7,15 @@ print_array(int *arr, int size)
 {
 int i;
 for (i = 0; i < size; i++)
-     {
-             if(i == 0) {
-                printf("%d", arr[i]);
-             }
-             else
-             {
-                     printf(", %d", arr[i]);
-             }
-     }
-     printf("\n");
-     
-     }
+{
+if(i == 0)
+{
+printf("%d", arr[i]);
+}
+else
+{
+printf(", %d", arr[i]);
+}
+}
+printf("\n"); 
+}
