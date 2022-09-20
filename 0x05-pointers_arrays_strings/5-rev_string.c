3b3b6d9e2bcc9678b0ee82350986281a6fa5f75a
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-19 19:18:08 -0700

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,10 +3,21 @@
 
 void rev_string(char *s) {
    
-    int i = 9;
+    int i = _arrlen(s);
+    i = i - 1;
     while(i >= 0) {
         _putchar(s[i]);
         i--;
     }
 }
 
+
+int
+_arrlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{}
+return (i);
+
+}
