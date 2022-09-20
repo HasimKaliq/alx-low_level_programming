27788c3866f46c798a2482ee6c41ed3966f240dc
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-19 17:44:29 -0700

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+
+/* Write a function that prints a string, followed by a new line, to stdout. */
+
+void
+print_rev(char *s)
+{
+int i = _strlen(s);
+while(i >= 0)
+{
+_putchar(s[i]);
+i--;
+}
+}
