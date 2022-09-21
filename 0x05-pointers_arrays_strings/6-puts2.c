a91bfaa85b9f64ff3707ef464c597335c0d3a232
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-20 17:12:55 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,15 @@
 #include "main.h"
 
 void
-puts2(char *s)
-{
-int i =  _strlen(s);
-int j;
-for (j = 0; j < i; j++)
-{
-if (s[j * 2] == '\0')
-{
-break;
-}
-_putchar(s[j * 2]);
-}
-_putchar('\n');
-}
-
-int
-_strlen(char *s)
+puts2(char *str)
 {
 int i;
-for (i = 0; s[i] != '\0'; i++)
-{}
-return (i - 1);
+
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
 }
