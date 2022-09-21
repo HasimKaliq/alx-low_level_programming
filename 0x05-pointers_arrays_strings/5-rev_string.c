8e4d41b29a232f6537a93bc84fb784e1f755a6ef
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-20 17:11:02 -0700

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,50 +1,27 @@
 
 #include "main.h"
 
-void rev_string(char *s)
+void
+rev_string(char *s)
 {
-	int i, j;
-	char str[1000];
+int i, j;
+char str[1000];
 
-	i = 0;
+i = 0;
 
-	while (*(s + i) != 0)
-	{
-		str[i] = *(s + i);
-		i++;
-	}
-
-	j = i - 1;
-	i = 0;
-
-	while (j >= 0)
-	{
-		*(s + j) = str[i];
-		j--;
-		i++;
-	}
-}
-
-
-/*
-void rev_string(char *s) {
-   
-    int i = _strlen(s);
-    
-    while(i >= 0) {
-        _putchar(s[i]);
-        i--;
-    }
-}
-
-
-int
-_strlen(char *s)
+while (*(s + i) != 0)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
-{}
-return (i - 1);
-
+str[i] = *(s + i);
+i++;
 }
-*/
+
+j = i - 1;
+i = 0;
+
+while (j >= 0)
+{
+*(s + j) = str[i];
+j--;
+i++;
+}
+}
