16de80adea4d32190ecd6efc3465d00953dfd101
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-21 19:29:57 -0700

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
+
 char *_strncat(char *dest, char *src, int n)
 {
 
-	int srcIndex = 0, dest_len = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[srcIndex++])
+	while (dest[index++])
 		dest_len++;
 
-	for(srcIndex = 0; src[srcIndex] && srcIndex < n; srcIndex++)
-		dest[dest_len++] = src[srcIndex];
-        
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
+
 }
