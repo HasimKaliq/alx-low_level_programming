6a0897988785995dc66bd56ac0007a07738160f7
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-22 00:30:55 -0700

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+void fizzBuzz(){
+    int i;
+    for(i = 1; i <= 100; i++) {
+        if(i % 3 == 0) {
+            printf("Fizz ");
+            continue;
+        }
+        else if(i % 5 == 0) {
+            printf("Buzz ");
+            continue;
+        }
+        
+        else if(i % 3 == 0 && i % 5 == 0){
+            printf("FizzBuzz ");
+            continue;
+        }
+        printf("%d ",i);
+        
+    }
+} 
