c2169f3e6d6a468899dbbec56d414ab44da70aed
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-21 20:42:49 -0700

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,8 @@ for (i = 0; i < 10; i++)
 for (j = 0; j <= 14; j++)
 {
 if (j >= 10)
-putchar((j / 10) + '0');
-putchar((j % 10) + '0');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
 }
 }
 return (0);
