f7cb595af14c3b7682151098c02687fdd3ee779c
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-21 18:59:03 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,16 @@
 
 #include "main.h"
 
-char *_strcat(char *dest, char *src) {
-    int destVal,srcVal;
-    
-    for(destVal = 0; dest[destVal] != '\0'; destVal++)
-    for(srcVal = 0; src[srcVal]; srcVal++) {
-        dest[destVal++] = src[srvVal];
-        return (dest);
-    }
+char *_strcat(char *dest, char *src)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
