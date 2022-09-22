c4721d03b943b2841ab651152fb5964576c4ace6
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-21 19:22:58 -0700

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+char *_strcat(char *dest, char *src)
+{
+
+	int srcIndex = 0, dest_len = 0;
+
+	while (dest[srcIndex++])
+		dest_len++;
+
+	while(srcIndex = 0; <= size)
+		dest[dest_len++] = src[srcIndex];
+        srcIndex++;
+	return (dest);
+}
