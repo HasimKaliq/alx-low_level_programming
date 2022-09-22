6e81833a6a1361d301b4b7f19b135c05a65453b0
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-22 12:13:18 -0700

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int counter,compVal;
-    
-    counter = 0;
-    
-    while (si[counter] == s2[counter] && s1[counter] != '\0') {
-        counter++;
-    }
-    compVal = s1[counter] - s2[counter];
-    return (compVal);
+	int counter, cmpVal;
+
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	{
+		counter++;
+	}
+
+	cmpVal = s1[counter] - s2[counter];
+	return (cmpVal);
 }
