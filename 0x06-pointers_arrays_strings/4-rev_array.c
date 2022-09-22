7cfbff8ec65082562d76eab278806f801f4e70d3
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-21 20:09:30 -0700

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,12 +1,14 @@
 
 #include "main.h"
 
-void reverse_array(int *a, int n) {
+void
+reverse_array(int *a, int n)
+{
 int start,end;
-for(end = n - 1; end > n/2; end --) {
-    start = a[n - 1 - end];
-    a[n - 1 - end] = a[end];
-    a[end] = start;
+for (end = n - 1; end > n/2; end --)
+{
+start = a[n - 1 - end];
+a[n - 1 - end] = a[end];
+a[end] = start;
 }
-
 }
