d9d9b4526a775c27d9158692b63d04b8ef79ebfc
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-22 00:44:43 -0700

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,13 @@ int main() {
 // Write C code here
 int i;
 for(i = 1; i <= 100; i++) {
-if(i % 3 == 0) {
+
+if(i % 3 == 0 && i % 5 == 0){
+printf("FizzBuzz ");
+continue;
+
+}
+else if(i % 3 == 0) {
 printf("Fizz ");
 continue;
 }
@@ -16,12 +22,8 @@ printf("Buzz ");
 continue;
 }
         
-else if(i % 3 == 0 && i % 5 == 0){
-printf("FizzBuzz ");
-continue;
 }
 printf("%d ",i);
         
-}
 return (0);
 }
