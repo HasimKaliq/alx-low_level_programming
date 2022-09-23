9968c7002b024c8e2b87fa9b52255407491e85a2
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-23 10:13:54 -0700

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,8 +1,7 @@
 
 #include "main.h"
 
-char
-*cap_string(char *s)
+char *cap_string(char *s)
 {
 int count;
 
@@ -14,11 +13,15 @@ if (s[0] >= 97 && s[0] <= 122)
 {
 s[0] = s[0] - 32;
 }
-if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'|| s[count] == ',' || s[count] == ';' || s[count] == '.'|| s[count] == '.' || s[count] == '!' || s[count] == '?'|| s[count] == '"' || s[count] == '(' || s[count] == ')'|| s[count] == '{' || s[count] == '}')
+if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+|| s[count] == '{' || s[count] == '}')
 {
 if (s[count + 1] >= 97 && s[count + 1] <= 122)
 {
-s[count + 1] = s[count the + 1] - 32;
+s[count + 1] = s[count + 1] - 32;
 }
 }
 count++;
