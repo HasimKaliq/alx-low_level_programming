dc566d26b9eb042409239fa5d9572b64a076e1d5
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-23 16:47:33 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
-char *leet(char *s)
+char
+*leet(char *s)
 {
-	int stringCount, leetCount;
-	char leetLetters[] = "aAeEoOtTlL";
-	char leetNums[] = "4433007711";
+int stringCount, leetCount;
+char leetLetters[] = "aAeEoOtTlL";
+char leetNums[] = "4433007711";
 
 /*  scan through string */
-	stringCount = 0;
-	while (s[stringCount] != '\0')
+stringCount = 0;
+while (s[stringCount] != '\0')
 /* check whether leetLetter is found */
-	{
-		leetCount = 0;
-		while (leetCount < 10)
-		{
-			if (leetLetters[leetCount] == s[stringCount])
-			{
-				s[stringCount] = leetNums[leetCount];
-			}
-			leetCount++;
-		}
-		stringCount++;
-	}
-	return (s);
+{
+leetCount = 0;
+while (leetCount < 10)
+{
+if (leetLetters[leetCount] == s[stringCount])
+{
+s[stringCount] = leetNums[leetCount];
+}
+leetCount++;
+}
+stringCount++;
+}
+return (s);
 }
