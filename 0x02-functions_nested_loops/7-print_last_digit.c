775e53a55286d0ca5a61e8ac41472dd8c2a6c013
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-23 10:36:53 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,24 @@
 #include "main.h"
-/*
- **
-*/
+
+int
+_abs(int n)
+{
+if (n >= 0)
+{
+		return (n);
+}
+else
+{
+		return (-1 * n);
+}
+}
+
 int
 print_last_digit(int n)
 {
-  int lastDigit = n % 10;
-  return (lastDigit);
-  return (0);
+int lastDigit;
+
+lastDigit = _abs((n % 10));
+_putchar(lastDigit + 48);
+return (lastDigit);
 }
