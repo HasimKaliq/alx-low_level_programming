b7ed488bb679e15923806075b79fba9eb473023f
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-23 16:50:43 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
-char *string_toupper(char *x)
+char
+*string_toupper(char *x)
 {
-	int length;
+int length;
 
-	length = 0;
+length = 0;
 
-	while (x[length] != '\0')
-	{
-		if (x[length] >= 97 && x[length] <= 122)
-		{
-			x[length] = x[length] - 32;
-		}
-		length++;
-	}
-	return (x);
+while (x[length] != '\0')
+{
+if (x[length] >= 97 && x[length] <= 122)
+{
+x[length] = x[length] - 32;
+}
+length++;
+}
+return (x);
 }
