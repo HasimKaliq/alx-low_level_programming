d6c75204d57d15f7e7e7c9481495e75b241af91f
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-28 19:34:30 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,17 @@
 
 #include "main.h"
 
-
+/**
+ * The program printsout a string using recursion
+ */
 void _puts_recursion(char *s)
 {
 
-    /*The base case.*/
-    if(*s == '\0'){
-        return; 
-    }
-    _putchar(*s);
-    _puts_recursion(++s);         
+/*The base case.*/
+if (*s == '\0')
+{
+return;
+}
+_putchar(*s);
+_puts_recursion(++s);
 }
