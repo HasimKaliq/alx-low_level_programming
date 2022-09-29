3024ec786bd14858d49daee3f5de12ec7cbeaba1
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-28 19:05:45 -0700

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@ void _puts_recursion(char *s)
 
     /*The base case.*/
     if(*s == '\0'){
-        return (_putchar('\n')); 
+        return; 
     }
     _putchar(*s);
     _puts_recursion(++s);         
