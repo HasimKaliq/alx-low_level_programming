fb9bdebc72cd69bfc796fc43351491313697f01c
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-09-29 18:27:02 -0700

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,13 +5,4 @@
 *  _strlen_recursion -  prints a string the length of a string.
 * @s: string to print
 */
-
-int _strlen_recursion(char *s)
-{
-/* The basecase */
-if(*s == '\0')
-    return (length);
-s++;
-
-_strlen_recursion(s);   
-}
+int length; 
