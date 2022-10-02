c20612060a07c5241731106ba0a3d9a84a8cbe9c
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-01 17:46:28 -0700

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,20 @@
+
+#include "main.h"
+
+char
+*_strcat(char *dest, char *src)
+{
+
+int indexSrc = 0, dest_len = 0;
+
+while (dest[indexSrc++])
+{
+dest_len++;
+}
+for (indexSrc = 0; src[indexSrc]; indexSrc++)
+{
+dest[dest_len++] = src[indexSrc];
+}
+dest[dest_len++] = '\0';
+return (dest);
+}
