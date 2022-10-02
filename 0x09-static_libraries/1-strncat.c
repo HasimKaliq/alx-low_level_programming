0b89fb8913b0f75490dbcab527d7b45b3ebcd6ad
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-01 17:53:19 -0700

    Create 1-strncat.c

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int srcIndex = 0, dest_len = 0;
+
+	while (dest[srcIndex++])
+		dest_len++;
+
+	for(srcIndex = 0; src[srcIndex] && srcIndex < n; srcIndex++)
+		dest[dest_len++] = src[srcIndex];
+        
+	return (dest);
+}
