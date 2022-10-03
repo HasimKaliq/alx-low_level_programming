d7ec7d52220af3824e1a3da556db209ba1417629
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-02 19:31:12 -0700

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    int i;
+    if(argc - 1 < 2) {
+        printf("Error");
+    }
+    else if(argc - 1 == 2) {
+        for(i = 1; i < argc-1; i++) {
+            int multiplaction = atoi(argv[i]) * atoi(argv[i+1]);
+            printf("%d", multiplaction);
+        }
+    }
+    return (0);
+}
