16c6fdd8267a82b7d7a176bff053909f4e7be746
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-02 19:04:04 -0700

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - expects a return of type int.
+ * @argc: size of the commands passed.
+ * @argv: an arrray that holds the commands.
+ * Return: return an int
+*/
+
+
 int main(int argc, char* argv[])
 {
 int i;
