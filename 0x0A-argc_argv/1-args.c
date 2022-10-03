4f0d6c9ecfd450b16edc620fb5070bb1c41d0356
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-02 18:16:18 -0700

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,7 @@
 #include "main.h"
-#include <stdio.h>
 
 int main(int argc, char __attribute__ ((unused)) *argv[]) {
-putchar(argc);
-putchar('\n');
+_putchar(argc);
+_putchar('\n');
 return (0);
 }
