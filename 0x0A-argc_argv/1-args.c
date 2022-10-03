48d5b9ba1c13d681d0979804cb9bccdec285dfcd
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-02 18:47:15 -0700

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char __attribute__ ((unused)) *argv[]) {
 
+
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
 if(argc > 1)
-{
 printf("%d\n", argc - 1);
-}
+
 else
-{
 printf("%d\n", argc - 1);
-}
+
 return (0);
+
 }
