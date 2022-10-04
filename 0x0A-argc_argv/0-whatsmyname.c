39150bfc047c291b3d3a41b5d4e04b57ee295d67
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-03 18:38:18 -0700

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints its name, followed by a new line.
+* @argc: an argument counter
+* @argv: an array that holds all arguments
+* Return: 0 if success
+*/
+
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
