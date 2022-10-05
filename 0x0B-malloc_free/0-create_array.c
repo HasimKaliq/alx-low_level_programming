5ca978d608b702a63acf96e27423b81585abade3
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-04 20:12:06 -0700

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,11 +5,11 @@ char *create_array(unsigned int size, char c)
 {
     char *s;
     unsigned int i;
-    
-    if (size == 0) {
-        return (NULL);
-    }   
+  
     s = malloc((size) * sizeof(char));
+    if (size == 0 || s == 0) {
+        return (NULL);
+    } 
     i = 0;
     while (i < size)
         {
