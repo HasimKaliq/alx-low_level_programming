7348e42a65c5fb0d7a22f1709a3817678b149bf2
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-05 18:38:09 -0700

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str) {
     
     char *new_memory;
     int i = 0;
-    new_memory = (char*)malloc(10 * sizeof(char));
+    new_memory = (char*)malloc(13 * sizeof(char));
     
     if(new_memory == NULL || str == NULL) {
         return (NULL);
