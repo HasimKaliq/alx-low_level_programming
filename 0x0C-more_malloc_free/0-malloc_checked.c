481231d168c0a7239e5a3ccb3dc734fbb4dec9a2
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-06 17:33:12 -0700

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * Write a function that allocates memory using malloc.
+ * Prototype: void *malloc_checked(unsigned int b);
+ * Returns a pointer to the allocated memory
+ * if malloc fails, the malloc_checked function should cause normal process termination with a status value of 98
+ */ 
+
+void *malloc_checked(unsigned int b) 
+{
+    unsigned int *memory;
+    memory = malloc(b * sizeof(unsigned int));
+    if(memory == NULL) {
+        return (98);
+    }
+    return (memory);
+}
