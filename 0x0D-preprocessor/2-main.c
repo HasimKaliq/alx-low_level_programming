9b0b6067a76bd1ac4f0b2c09e473c7f07b3ff1a9
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-10 19:26:05 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,7 +4,7 @@
  * main - prints the file name the c file was compiled from
  * @argc: a variable counter for commands
  * @argv: an array of char for commands that was passed in
- * Return - returns 0 if success else 1
+ * Return: returns 0 if success else 1
 */
 
 int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[])
