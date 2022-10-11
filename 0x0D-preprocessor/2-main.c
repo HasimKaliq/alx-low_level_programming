fec61ac268e68ad14488a58ec699072fc6f0fa7f
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-10 19:24:19 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[]) {
+/**
+ * main - prints the file name the c file was compiled from
+ * @argc: a variable counter for commands
+ * @argv: an array of char for commands that was passed in
+ * Return - returns 0 if success else 1
+*/
+
+int main(int __attribute__ ((unused)) argc, char __attribute__ ((unused)) *argv[])
+{
 	printf("%s\n", __FILE__);
 	return (0);
 }
