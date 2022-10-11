5dce685a2991c8f5032d6d431e8c0e462302bd69
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-10 19:12:20 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int __undefined__(unused) argc, char *argv[]) {
+int main(int __undefined__(unused)argc, char *argv[]) {
 	printf("program's name is: %s\n",argv[0]);
 	return 0;
 }
