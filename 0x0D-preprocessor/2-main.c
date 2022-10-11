2e2d7a0d680200a3f2c4a75e8fa87b7913da484e
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-10 18:53:45 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
 
+
+
+
+int main(int argc, char const *argv[])
+{
     /* code */
-printf("Executable name: %s\n", __FILE__);
+    printf("Executable name: %s\n", argv[0]);
+    return (0);
+}
