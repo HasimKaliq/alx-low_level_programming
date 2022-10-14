642b6b5a05c1678f6cfb6af6d654aabf474a01f1
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-13 17:41:39 -0700

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,25 +5,21 @@
  * Return: Always 0.
  */
 
-
-
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int sum, i;
+unsigned int sum, i;
 
-    /* This gathers all the argunments va_list name of list */
-    va_list args; 
+/* This gathers all the argunments va_list name of list */
+va_list args; 
 
-    if(n == 0)
-        return (0);
-    /* va_start gets the first compulsory argunment va_start(args, n == is the varibale for the compulsory) */
-    va_start(args, n); 
+if (n == 0)
+return (0);
+/* va_start gets the first compulsory argunment va_start(args, n == is the varibale for the compulsory) */
+va_start(args, n); 
 
-    /* var_args this takes in all the argunments and the data type. */
-    for(i = 0; i < n; i++)
-    {
-        sum+= va_arg(args, int); 
-    }
-    return (sum);
+/* var_args this takes in all the argunments and the data type. */
+for (i = 0; i < n; i++)
+sum += va_arg(args, int); 
+return (sum);
 }
 
