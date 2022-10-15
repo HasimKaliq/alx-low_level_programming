fd18f47e39e85c3b58f292e7a54fd6d457a615dd
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-14 19:18:32 -0700

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,4 @@
 #include "variadic_functions.h"
-
-
 /**
  * print_numbers - prints numbers, followed by a new line
  * @separator: separator string
@@ -8,11 +6,10 @@
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
-{   
+{
 unsigned int i;
 va_list args;
 va_start(args, n);
-
 for (i = 0; i < n; i++)
 {
 int numbers = va_arg(args, int);
