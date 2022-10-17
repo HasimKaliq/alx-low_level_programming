12e107a929f8a00e1c771ddd399bbe38f2bd8e22
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-16 17:54:06 -0700

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,16 +15,16 @@ int counter, i;
 
 if (s1 == NULL)
 {
-s1 = " ";
+        s1 = "";
 }
 
 if (s2 == NULL)
 {
-s2 = " ";
+        s2 = "";
 }
 
 for (i = 0; s1[i] != '\0'; i++);
-memory = malloc(i * sizeof(char) + 1);
+memory = malloc(sizeof(char));
 
 if (memory == NULL)
 {
