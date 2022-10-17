313f6836704bdc841dcdeb4223352c6ff81e0282
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-16 17:50:40 -0700

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,28 +5,39 @@
  * str_concat - the function return an address of a concatinated string
  * @s1: first character
  * @s2: second character
- * Return: the address of the new memory
- */
+ * Return: the address of the new memory   
+ */ 
 
 char *str_concat(char *s1, char *s2)
 {
 char *memory;
 int counter, i;
- 
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
+    
+if (s1 == NULL) {
+        s1 = " ";
+}
+
+if (s2 == NULL) {
+        s2 = " ";
+}
+
 for (i = 0; s1[i] != '\0'; i++);
-memory = malloc(sizeof(char));
-if (memory == NULL)
-return (NULL);
-for (counter = 0; s1[counter] != '\0'; counter++)
-memory[counter] = s1[counter];
-for (counter = 0; s2[counter] != '\0'; counter++)
-memory[i] = s2[counter];
-i++;
+memory = malloc(i * sizeof(char) + 1);
+
+if (memory == NULL) {
+        return (NULL);
+}
+    
+for (counter = 0; s1[counter] != '\0'; counter++) {
+        memory[counter] = s1[counter];
+}
+
+
+for (counter = 0; s2[counter] != '\0'; counter++) {
+        memory[i] = s2[counter];
+        i++;
+}
 memory[i] = '\0';
 return (memory);
-free(memory);
 }
+
