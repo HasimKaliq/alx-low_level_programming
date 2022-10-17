493d4494e6db13a74c12dda15019d5a5fb3a0f8e
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-16 17:48:31 -0700

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,26 +12,20 @@ char *str_concat(char *s1, char *s2)
 {
 char *memory;
 int counter, i;
-    
+ 
 if (s1 == NULL)
 s1 = "";
-
 if (s2 == NULL)
 s2 = "";
-
 for (i = 0; s1[i] != '\0'; i++);
 memory = malloc(sizeof(char));
 if (memory == NULL)
 return (NULL);
-    
-for(counter = 0; s1[counter] != '\0'; counter++)
+for (counter = 0; s1[counter] != '\0'; counter++)
 memory[counter] = s1[counter];
-
-
-for(counter = 0; s2[counter] != '\0'; counter++)
+for (counter = 0; s2[counter] != '\0'; counter++)
 memory[i] = s2[counter];
 i++;
-
 memory[i] = '\0';
 return (memory);
 free(memory);
