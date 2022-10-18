12b2d09bea0f931c7ef108721351fb1e98eede3f
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-17 19:27:35 -0700

    Update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 
 int **alloc_grid(int width, int height){
+    int *memory;
+    
     if((width <= 0) || (height <= 0)) {
         return (NULL);
     }
-    int *memory = malloc(sizeof(int));
+    
+    memory = malloc(sizeof(int));
     if(memory == NULL){
         return (NULL);
     }
