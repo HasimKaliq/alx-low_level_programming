f5b8b16b6c77e7449806914a8b19b2c4227af110
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-21 19:14:26 -0700

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -21,12 +21,11 @@ typedef struct list_s
 
 
 int print_list(const list_t *h) {
-
+    int count;
     if(h->str == NULL) {
         printf("[0] (nil)");
     }
 
-    int count;
     while(h->next != NULL) {
         printf("[%d] %s\n", h->len, h->str);
         h = h->next;
