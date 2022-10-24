9ae2929e4cb3e89019e3301a30250e37849adb8a
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-24 01:00:17 -0700

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,13 +1,19 @@
 #include "lists.h"
 
-size_t list_len(const list_t *h) {
+/**
+ * @h: is a pointer of type list_t
+ * list_len - prints num of elements of a list_t list.
+ * Return - the number of elements.
+*/
 
-    int counter;
-    counter = 0;
-    while (h != NULL) {
-        h = h->next;
-        counter++;
-        
-    }
-    return (counter);
+size_t list_len(const list_t *h)
+{
+int counter;
+counter = 0;
+while (h != NULL)
+{
+h = h->next;
+counter++;
+}
+return (counter);
 }
