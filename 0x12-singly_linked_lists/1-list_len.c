6a036c1285533aaae5d2af615709d44778c6d84f
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-24 00:44:08 -0700

    Create 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,11 @@
+size_t list_len(const list_t *h) {
+
+    int counter;
+    counter = 0;
+    while (h != NULL) {
+        h = h->next;
+        counter++;
+        
+    }
+    return (counter);
+}
