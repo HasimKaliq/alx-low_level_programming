e5735126b8ca9ca480b68570f4a8955d120c7e71
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-24 00:53:02 -0700

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@ size_t print_list(const list_t *h) {
         printf("[0] (nil)");
     }
 
-    count = 1;
+    count = 0;
     while(h != NULL) {
         printf("[%d] %s\n", h->len, h->str);
         h = h->next;
