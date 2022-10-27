56e191d3470b213eae209d05be125e48e192ac52
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-10-26 18:04:22 -0700

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,11 @@
 
+/**
+ * add_mode - returns the added node in a linked list_t list
+ * @head: head of linked list
+ * @str: string to be duplicated
+ * Return: number of elements in list_t
+ */
+
 #include "lists.h"
 
 list_t *add_node(list_t **head, const char *str)
