4f8d52d4ccbe9a96f014a3591a1dbf6a617e5229
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-04 15:47:13 -0700

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,17 +8,32 @@
 
 #include "lists.h"
 
+int _strlen(char *s) {
+    
+    char *p = s;
+    while (*s)
+        s++;
+    return (s-p);
+}
+
+
 list_t *add_node(list_t **head, const char *str)
 {
+    char *str_cpy;
     list_t *first_node;
-    first_node = (list_t *) malloc(sizeof(list_t));
+    first_node = malloc(sizeof(list_t));
 
     if(first_node == NULL) {
         return (NULL);
     }
-    first_node -> str = strdup(str);
+    str_cpy = strdup(str);
+    if(str_cpy == NULL)
+        free(first_node);
+        return (NULL);
+    first_node -> len = _strlen(str_cpy);    
+    first_node -> str = str_cpy;
     first_node ->next = *head;
     *head = first_node;
 
-    return (first_node);
+    return (head);
 }
