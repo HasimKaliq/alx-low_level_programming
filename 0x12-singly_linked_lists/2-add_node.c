b8d26dfd4396cef606afbae60124039cc942289f
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-05 06:56:05 -0700

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,14 +11,18 @@
 list_t *add_node(list_t **head, const char *str)
 {
   
-
+int i;
+char *newstr;
+newstr = strdup(str);
+for(i = 0; newstr[i] != '\0'; ++i);
 
 list_t *first_node;
 first_node = malloc(sizeof(list_t));
 
 if(first_node == NULL)
   return (NULL);
-first_node -> str = strdup(str);
+first_node -> len = i;
+first_node -> str = newstr;
 first_node ->next = *head;
 *head = first_node;
 return (*head);
