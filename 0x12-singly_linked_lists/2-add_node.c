ce10fed0e54a90b227d127e5147e6c08d3c8f667
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-05 08:24:09 -0700

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,14 +9,7 @@
 #include "lists.h"
 
 
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
-	return (i);
-}
 
 
 list_t *add_node(list_t **head, const char *str)
@@ -28,13 +21,7 @@ list_t *add_node(list_t **head, const char *str)
         return (NULL);
     }
     new_node->str = strdup(str);
-    if(new_node->str == NULL) {
-        free(new_node);
-        return(NULL);
-    }
-    new_node->len = _strlen(new_node->str);
-    new_node->next = *head;
-    
+    new_node->next = *head;    
     *head = new_node;
     return(*head);
 }
