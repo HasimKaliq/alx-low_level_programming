b63f3b0ea31b1421019f5e185e18b5a42343be29
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-05 20:25:19 -0700

    Create 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - Add a node at the end of the list
+ * @head: a pointer to a pointer of type list_t
+ * @str: the string to be copied into the new node
+ * Return: the address of the new element, or NULL if it failed
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *end_node;
+char *dup;
+int count;
+list_t *last;
+end_node = malloc(sizeof(list_t));
+if(end_node == NULL)
+{
+return (NULL);    
+}
+last = *head;
+dup = strdup(str);
+if (dup == NULL)
+{
+free(end_node);
+return(NULL);
+}
+for(count = 0; str[count] != '\0'; count++);   
+while (last->next != NULL)
+{
+last = last->next;
+}
+last->str = dup;
+last->len = count;
+last->next = NULL;
+return (last);
+}
