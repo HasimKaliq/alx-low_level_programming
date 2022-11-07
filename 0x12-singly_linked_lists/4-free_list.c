14e6949148bb14f261a9a55d3c34510b9521a2c8
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-07 20:55:05 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,16 +5,13 @@
  * @head: a pointer to a pointer of type list_t
 */
 
-void free_list(list_t *head)
-{
-list_t temp;
-if(head == NULL)
-{
-return(NULL);
-}
-while (head != NULL)
-{
-temp = head->next;
-free(temp);
-}
+void free_list(list_t *head){
+    list_t temp;
+    
+    
+    while(head != NULL){
+        temp = head -> next;
+        free(head);
+        head = temp;
+    }
 }
