6d1c20d0a5ddf47ca76e1a247364c044db1bbf1a
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-07 21:01:46 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,12 +6,15 @@
 */
 
 void free_list(list_t *head){
-    list_t temp;
-    
-    
-    while(head != NULL){
-        temp = head -> next;
-        free(head);
-        head = temp;
-    }
+list_t temp;
+if (head == NULL)
+{
+return (NULL);
+}
+while (head != NULL)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
