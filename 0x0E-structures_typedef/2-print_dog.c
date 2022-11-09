53f741fcd878620bde3ac57d19f17e4d46d33411
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-08 18:15:40 -0800

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,15 +6,24 @@ void print_dog(struct dog *d)
 if(d->name == NULL)
 {
 printf("(nil)\n");
+printf("Age: %f\n", d->age);
+printf("Owmer: %s\n", d->owner);
 }
+
 if(d->age == NULL)
 {
-printf("(nil)\n");
+printf("Name: %s\n", d->name);
+printf("(nil)\n", d->age);
+printf("Owmer: %s\n", d->owner);
 }
+
 if(d->owner == NULL)
 {
-printf("(nil)\n");
+printf("Name: %s\n", d->name);
+printf("Age: %f\n", d->age);
+printf("(nil)\n", d->owner);
 }
+
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owmer: %s\n", d->owner);
