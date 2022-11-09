e01c0ed8045ebc64bd7f18d9f6620c445d499d93
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-08 17:34:32 -0800

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,10 @@
 #ifndef DOG_H
 #DOG_H
 /**
- * @name: is a varible of type char
- * @age: is a variable of type int
- * @owner: is a variable of type char
+* struct dog - is a struct of type doog
+* @name: is a varible of type char
+* @age: is a variable of type int
+* @owner: is a variable of type char
 */
 struct dog
 {
