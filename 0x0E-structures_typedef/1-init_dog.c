3f355df8b1f8ee271c79f48bbe046447db4f9718
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-08 18:00:11 -0800

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,7 +7,8 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+struct d dog;
+dog->name = d->name;
+dog->age = d->age;
+dog->owner = d->owner;
 }
