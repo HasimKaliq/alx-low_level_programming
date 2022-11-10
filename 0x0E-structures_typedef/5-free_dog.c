fde8b7e9ece3f298d562665b68c90072dfb453a4
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-10 13:21:52 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,6 +1,11 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * free_dog - a function that returns nothing 
+ * @d: is a pointer variable of type dog type struct
+*/
 void free_dog(dog_t *d)
 {
 if (d == NULL)
