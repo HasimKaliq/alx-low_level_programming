2566e50ea2560a00b09ab874159cdbc27fc72068
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-10 17:21:17 -0800

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,4 +3,17 @@
 
 size_t print_listint(const listint_t *h)
 {
+  int count;
+  listint_t *temp = h;
+  
+  if(h == NULL)
+  {
+    return (NULL);
+  }
+  while(temp != NULL)
+  {
+    temp = h->next;
+    count++;
+  }
+  return (count);
 }
