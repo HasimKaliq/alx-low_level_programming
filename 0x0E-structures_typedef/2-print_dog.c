6e1536ac3de2b1cb0fbe29b248098365aba9c55c
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-10 16:07:55 -0800

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,8 +4,6 @@
  * print_dog - a function with which returns void
  * @d: is a pointer variable of type dog type struct
 */
-
-
 void print_dog(struct dog *d)
 {
 if (d == NULL)
@@ -15,25 +13,13 @@ return;
 if (d->name == NULL)
 {
 printf("(nil)\n");
-printf("Age: %f\n", d->age);
-printf("Owmer: %s\n", d->owner);
 }
 if (d->age < 1)
 {
-printf("Name: %s\n", d->name);
 printf("(nil)\n");
-printf("Owmer: %s\n", d->owner);
 }
 if (d->owner == NULL)
 {
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
 printf("(nil)\n");
-}
-else
-{
-printf("Name: %s\n", d->name);
-printf("Age: %f\n", d->age);
-printf("Owmer: %s\n", d->owner);
 }
 }
