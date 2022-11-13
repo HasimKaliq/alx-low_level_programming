cd001fcb87957de53aca8656f221bec6f5ccabe8
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-13 08:00:03 +0100

    Create 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+    listint_t *new_node = NULL;
+    new_node = malloc(sizeof(listint_t);
+    if (new_node == NULL)
+    {
+        return (NULL);
+    }
+    
+    
+    if (*head == NULL)
+    {
+        new_node->n = n;
+        new_node->next = NULL
+        *head = new_node;
+    }
+    
+    new_node->n = n;
+    new_node->next = *head;
+    *head = new_node;
+    return(*head);
+}
