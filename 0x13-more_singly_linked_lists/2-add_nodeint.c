5a7f740d7395c113d5b8e7df0e3a3e4444b14ffd
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-13 08:48:21 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,12 +9,6 @@ if (new_node == NULL)
 {
 return (NULL);
 }
-if (*head == NULL)
-{
-*head = new_node;
-new_node->n = n;
-new_node->next = NULL;
-}
 new_node->n = n;
 new_node->next = *head;
 *head = new_node;
