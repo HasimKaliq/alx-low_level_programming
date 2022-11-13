b4b6a2c73a526b339cdadd257440a70039dbceac
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-13 09:31:47 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
 #include "lists.h"
 
 size_t print_listint(const listint_t *h)
 {
-  if (h == NULL){
-    return(1);
-  }
   int count;
   listint_t temp = h;
   
   while(temp != NULL)
   {
+    printf("%d", temp->n);
     temp = temp->next;
     count++;
   }
