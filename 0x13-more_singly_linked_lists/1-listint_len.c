7c12af60d0a1763752c3e00d635b30f0eb3e8319
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-13 11:21:17 +0100

    Create 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+size_t listint_len(const listint_t *h)
+{
+size_t count = 0;
+if (h == NULL)
+{
+return (count);
+}
+while (h)
+{
+if(h->n)
+{
+count++;
+}
+}
+return (count);
+}
