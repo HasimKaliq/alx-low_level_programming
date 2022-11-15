b31116dd9c9e617980633b6c50f39863637f099e
Hasim Kaliq <104144449+HasimKaliq@users.noreply.github.com>
2022-11-16 00:25:55 +0100

    Update 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,18 +6,21 @@ listint_t *end_node;
 listint_t *temp
 temp = *head;
 end_node = malloc(sizeof(listint_t));
+end_node->n = n;
+end_node->next = NULL;
 
 if (end_node == NULL)
 {
 return (NULL);
 }
-end_node->n = n;
-end_node->next = NULL;
-
-while (temp)
+if(*head == NULL)
+{
+*head = end_node;
+}
+while (temp->next != NULL)
 {
 temp = temp->next;
 }
 temp->next = end_node;
-return(temp);
+return(end_node);
 }
