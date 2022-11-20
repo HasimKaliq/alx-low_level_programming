805c9190be1f401130180fe7a7536ce0064326e1
Hasim Kaliq <hasimkaliq@gmail.com>
2022-11-20 02:17:03 -0800

    fixed the betty issue

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,9 @@
 #include "lists.h"
 
+/**
+ * print_listint - returns the elements in the list
+ * @h: a variable of type listint_t
+ * Return - the number of nodes.*/
 size_t print_listint(const listint_t *h)
 {
 size_t count = 0;
