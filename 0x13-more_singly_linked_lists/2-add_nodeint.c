723197ca83c80c55f21d70ea658b26b2ab051fd4
Hasim Kaliq <hasimkaliq@gmail.com>
2022-11-20 03:28:50 -0800

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * print_listint - add node to the start of a list.
+ * @head: a variable of type listint_t
+ * @n: the value to be stored
+ * Return: the new of node.
+ */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
