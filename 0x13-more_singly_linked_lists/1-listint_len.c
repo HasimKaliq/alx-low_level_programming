d2af76fc9a87a147e76de382edb3fee66fb6a7d5
Hasim Kaliq <hasimkaliq@gmail.com>
2022-11-20 04:52:55 -0800

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,17 +7,10 @@
 size_t listint_len(const listint_t *h)
 {
 size_t count = 0;
-if (h == NULL)
-{
-return (1);
-}
 while (h)
-{
-if (h->n  || h->next != NULL)
 {
 count++;
 h = h->next;
-}
 }
 return (count);
 }
