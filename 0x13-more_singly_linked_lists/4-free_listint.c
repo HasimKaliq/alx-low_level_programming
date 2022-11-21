8610e00208265227f8ff82b28e4b1f1b9bcecf57
Hasim Kaliq <hasimkaliq@gmail.com>
2022-11-20 17:26:12 -0800

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,15 +2,13 @@
 
 void free_listint(listint_t *head)
 {
-listint_t *temp;
 if (head == NULL)
 {
 return (NULL);
 }
 while(head)
 {
-temp = head;
-free(temp);
 head = head->next;
+free(head);
 }
 }
