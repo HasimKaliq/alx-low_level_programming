fec723c5487e8718e6db9fd38c50ff03699712c0
Hasim Kaliq <hasimkaliq@gmail.com>
2022-11-21 16:20:12 -0800

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,8 @@
 #include "lists.h"
-
-
 /**
-* free_listint = a function that frees a listint_t list.
-* @head: a pointer variable of type listint
+ * free_listint = a function that frees a listint_t list.
+ * @head: a pointer variable of type listint
 */
-
 void free_listint(listint_t *head)
 {
 listint_t *current;
