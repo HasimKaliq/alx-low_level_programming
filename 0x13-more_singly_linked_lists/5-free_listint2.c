36bfac8eec0b7297912fd6c69be345f9035677e0
Hasim Kaliq <hasimkaliq@gmail.com>
2022-11-21 17:02:43 -0800

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,12 @@ void free_listint2(listint_t **head)
 {
 listint_t *temp;
 listint_t *next;
-temp = *head
+temp = *head;
 while (temp != NULL)
 {
 	next = temp->next;
 	free(temp);
 	temp = next;
 }
+*head = NULL;
 }
