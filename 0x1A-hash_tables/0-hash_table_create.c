159881383725269ebe1b025295a419fe7f813225
Hasim Kaliq <hasimkaliq@gmail.com>
2023-01-06 23:55:44 -0800

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,15 +3,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_node_t *hash_table;
-    int *addr_of_hash_table;
-    hash_table = malloc((size) * sizeof(hash_node_t));
-    addr_of_hash_table = &hash_table;
-    if(hash_table == NULL){
-        return NULL;
-    }
-    else{
-        return(addr_of_hash_table);
-    }
+hash_table_t *table;
+table =  malloc(sizeof(hash_node_t));
+if (table == NULL)
+{
+    return (NULL);
 }
-
+table -> size = size;
+table -> array = table;
+return (table);
+}
